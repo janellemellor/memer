38edcdb68178014e86234dc0797c7b733918263e
janelle mellor <janellemellor@gmail.com>
2020-04-04 10:38:22 -0700

    add delete test

--- __tests__/memes.test.js
+++ __tests__/memes.test.js
@@ -60,5 +60,15 @@ describe('meme routes', () => {
       });
   });
 
+  it('deletes a meme', async() => {
+    const meme = await getMeme();
+
+    return request(app)
+      .delete(`/api/v1/memes/${meme._id}`)
+      .then(res => {
+        expect(res.body).toEqual(meme);
+      });
+  });
+
 });
 // * `DELETE /api/v1/memes/:id` to delete a meme
