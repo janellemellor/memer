8ce28347f3dd576254f11ef40263e374f4c0f4d1
janelle mellor <janellemellor@gmail.com>
2020-04-04 10:32:58 -0700

    add patch test

--- __tests__/memes.test.js
+++ __tests__/memes.test.js
@@ -46,7 +46,20 @@ describe('meme routes', () => {
       });
   });
 
+  it('updates a meme', async() => {
+    const meme = await getMeme();
+
+    return request(app)
+      .patch(`/api/v1/memes/${meme._id}`)
+      .send({ top: 'goodbye' })
+      .then(res => {
+        expect(res.body).toEqual({
+          ...meme,
+          top: 'goodbye'
+        });
+      });
+  });
+
 });
-// * `GET /api/v1/memes/:id` to get a meme by id
 // * `PUT /api/v1/memes/:id` to update a meme
 // * `DELETE /api/v1/memes/:id` to delete a meme
