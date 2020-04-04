80208973d7f2559603c095177b3f808f095e50dc
janelle mellor <janellemellor@gmail.com>
2020-04-04 09:56:59 -0700

    update seed data

--- db/seed.js
+++ db/seed.js
@@ -1,10 +1,10 @@
 const Meme = require('../lib/models/Meme');
+const chance = require('chance').Chance();
 
-module.exports async({ memesToCreate = 25 } = {}) => {
-
-    const memes = await Meme.create([...Array(memesToCreate)].map(() => ({
-        top: ,
-        image: ,
-        bottom: ,
-    }))); 
+module.exports = async({ memesToCreate = 25 } = {}) => {
+  await Meme.create([...Array(memesToCreate)].map(() => ({
+    top: chance.animal(),
+    image: chance.url({ path: 'images' }),
+    bottom: chance.sentence(5),
+  }))); 
 };
